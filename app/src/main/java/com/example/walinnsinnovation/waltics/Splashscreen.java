81ba71716b1fe3a02394b66b4404f8b46f2b15bd
Pinky <31914628+Rejoyline@users.noreply.github.com>
2018-01-08 17:25:40 +0530

    initial commit

---
+++ app/src/main/java/com/example/walinnsinnovation/waltics/Splashscreen.java
@@ -0,0 +1,16 @@
+package com.example.walinnsinnovation.waltics;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        if(getSupportActionBar()!=null){
+            getSupportActionBar().hide();
+        }
+    }
+}
