f123687dff2887b512bf88ac1da93a90de47c69f
PrettyAndroid <rejoylin2015@gmail.com>
2018-03-07 13:11:52 +0530

    version 7 uploaded

--- app/src/main/java/com/walinns/walinnsinnovation/waltics/MyApplication.java
+++ app/src/main/java/com/walinns/walinnsinnovation/waltics/MyApplication.java
@@ -4,11 +4,20 @@ import android.app.Application;
 import android.content.Context;
 import android.support.multidex.MultiDex;
 
+import com.clevertap.android.sdk.ActivityLifecycleCallback;
+
 /**
  * Created by walinnsinnovation on 16/01/18.
  */
 
 public class MyApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        ActivityLifecycleCallback.register(this);
+        super.onCreate();
+    }
+
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
